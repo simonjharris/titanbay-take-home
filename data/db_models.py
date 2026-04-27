73285a4a12727f7b8cec476b271d10628e3e46a5
Simon Harris <simonjharris90@gmail.com>
2026-04-27 10:41:09 +0100

    Add missing db models and generate alembic migration.

--- data/db_models.py
+++ data/db_models.py
@@ -1,17 +1,18 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, date
 from decimal import Decimal
 
-from sqlalchemy import func, Column, UUID, Enum
+from sqlalchemy import func, Column, UUID, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
-from data.types import FundStatus
+from data.types import FundStatus, InvestorType
 
 
 class Base(DeclarativeBase):
     id: Mapped[uuid.UUID] = Column(UUID(as_uuid=True), primary_key=True, default=lambda: uuid.uuid4())
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(),server_onupdate=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), server_onupdate=func.now())
+
 
 class Fund(Base):
     __tablename__ = "funds"
@@ -19,3 +20,18 @@ class Fund(Base):
     vintage_year: Mapped[int]
     target_size_usd: Mapped[Decimal]
     status: Mapped[FundStatus]
+
+
+class Investor(Base):
+    __tablename__ = "investors"
+    name: Mapped[str]
+    investor_type: Mapped[InvestorType]
+    email: Mapped[str]
+
+
+class Investment(Base):
+    __tablename__ = "investments"
+    fund_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("funds.id"))
+    investor_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("investors.id"))
+    amount_usd: Mapped[Decimal]
+    investment_date: Mapped[date]
