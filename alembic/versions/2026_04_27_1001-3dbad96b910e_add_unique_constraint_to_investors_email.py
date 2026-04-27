3ecac97b94fb698a64a63151f2efc81c4f993071
Simon Harris <simonjharris90@gmail.com>
2026-04-27 11:33:30 +0100

    Add unique constraint to email.

---
+++ alembic/versions/2026_04_27_1001-3dbad96b910e_add_unique_constraint_to_investors_email.py
@@ -0,0 +1,26 @@
+"""add unique constraint to investors email
+
+Revision ID: 3dbad96b910e
+Revises: f0e32b9c1338
+Create Date: 2026-04-27 10:01:52.466552+00:00
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '3dbad96b910e'
+down_revision: Union[str, Sequence[str], None] = 'f0e32b9c1338'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_unique_constraint("uq_investors_email", "investors", ["email"])
+
+
+def downgrade() -> None:
+    op.drop_constraint("uq_investors_email", "investors", type_="unique")
